6008e7c95226f00267c1ef03f967f420fe743224
dislersd <dislersd@gmail.com>
2020-08-01 13:34:56 -0700

    refactored code - changed farms graph - using axios to fetch data

--- createGraph.js
+++ createGraph.js
@@ -1,73 +1,367 @@
-const data = require("./data_from_api.json");
+// const data = [
+//   {
+//     "farm_name": "Karabin Farms",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "7243",
+//     "website": {
+//       "url": "http://www.karabinfarms.com"
+//     },
+//     "zipcode": "06489",
+//     "phone1": "860-620-0194",
+//     "business": "Karabin Farms",
+//     "l": "13",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.82920259099967, 41.61609643000048]
+//     },
+//     "location_1_address": "894 Andrews Street",
+//     "location_1_city": "Southington",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06489"
+//   },
+//   {
+//     "farm_name": "SCOTT'S ORCHARD & NURSERY",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "8320",
+//     "website": {
+//       "url": "http://www.scottsorchardandnursery.com"
+//     },
+//     "zipcode": "06033",
+//     "phone1": "860-633-8681",
+//     "business": "SCOTT'S ORCHARD & NURSERY",
+//     "l": "25",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.56006305799963, 41.68711478200049]
+//     },
+//     "location_1_address": "1838 New London Turnpike",
+//     "location_1_city": "Glastonbury",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06033"
+//   },
+//   {
+//     "farm_name": "Gardner Farms",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "13396",
+//     "zipcode": "06281-3025",
+//     "phone1": "860-974-2471",
+//     "business": "Gardner Farms",
+//     "l": "13",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.03418229599964, 41.9996829800005]
+//     },
+//     "location_1_address": "109 Redhead Hill Road",
+//     "location_1_city": "Woodstock",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06281-3025"
+//   },
+//   {
+//     "farm_name": "Smoke and Mirrors Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "9878",
+//     "zipcode": "06482",
+//     "business": "Smoke and Mirrors Farm",
+//     "l": "22",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-73.28371050499965, 41.44197777000045]
+//     },
+//     "location_1_address": "120 Walnut Tree Hill Road",
+//     "location_1_city": "Sandy Hook",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06482"
+//   },
+//   {
+//     "farm_name": "Wright's Orchard & Dried Flower Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "9135",
+//     "website": {
+//       "url": "http://www.wrightsorchard.com"
+//     },
+//     "zipcode": "06084",
+//     "phone1": "860-872-1665",
+//     "business": "Wright's Orchard & Dried Flower Farm",
+//     "l": "36",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.31161142799965, 41.86535803200047]
+//     },
+//     "location_1_address": "271 South River Road",
+//     "location_1_city": "Tolland",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06084"
+//   },
+//   {
+//     "farm_name": "Second Wind Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "16567",
+//     "zipcode": "06795",
+//     "phone1": "860-747-7717",
+//     "business": "Second Wind Farm",
+//     "l": "16",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-73.12461718299966, 41.650966936000486]
+//     },
+//     "location_1_address": "180 Apple Hill Drive",
+//     "location_1_city": "Watertown",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06795"
+//   },
+//   {
+//     "farm_name": "Hard Rain Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "10102",
+//     "zipcode": "06013",
+//     "phone1": "860-675-3941",
+//     "business": "Hard Rain Farm",
+//     "l": "14",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.93239979899965, 41.77950381900047]
+//     },
+//     "location_1_address": "57 Spielman Highway, Rte. 4",
+//     "location_1_city": "Burlington",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06013"
+//   },
+//   {
+//     "farm_name": "Turtle Ledge Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "8803",
+//     "zipcode": "06247",
+//     "phone1": "860-455-1411",
+//     "business": "Turtle Ledge Farm",
+//     "l": "17",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.03881996499967, 41.748564038000495]
+//     },
+//     "location_1_address": "460 Windham Road",
+//     "location_1_city": "Hampton",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06247"
+//   },
+//   {
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "15260",
+//     "zipcode": "06354-1724",
+//     "phone1": "860-564-8733",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-71.86814770599966, 41.758510396000474]
+//     },
+//     "location_1_address": "701 Squaw Rock Road",
+//     "location_1_city": "Moosup",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06354-1724"
+//   },
+//   {
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "2839",
+//     "zipcode": "06790",
+//     "phone1": "860-482-6507",
+//     "business": "Andrea P Lippincott",
+//     "l": "0",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-73.14842241099967, 41.80255713000048]
+//     },
+//     "location_1_address": "613 Highland Ave",
+//     "location_1_city": "Torrington",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06790"
+//   },
+//   {
+//     "farm_name": "Stevens Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "18248",
+//     "zipcode": "06241",
+//     "phone1": "860-774-3006",
+//     "business": "Stevens Farm",
+//     "l": "12",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-71.82521253299967, 41.83746244200046]
+//     },
+//     "location_1_address": "186 Slater Hill Road",
+//     "location_1_city": "Dayville",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06241"
+//   },
+//   {
+//     "farm_name": "Dondero Orchards, LLC",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "6449",
+//     "website": {
+//       "url": "http://www.donderoorchards.com"
+//     },
+//     "zipcode": "06073",
+//     "phone1": "860-659-0294",
+//     "business": "Dondero Orchards, LLC",
+//     "l": "21",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.56263587899963, 41.656721794000475]
+//     },
+//     "location_1_address": "529 Woodland Street",
+//     "location_1_city": "South Glastonbury",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06073"
+//   },
+//   {
+//     "farm_name": "Maywood Farm/G.L. Bollard Corp.",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "3107",
+//     "zipcode": "06752",
+//     "phone1": "860-354-4104",
+//     "business": "Maywood Farm/G.L. Bollard Corp.",
+//     "l": "31",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-73.34392587399964, 41.489026400000455]
+//     },
+//     "location_1_address": "52 Cooper Rd",
+//     "location_1_city": "Bridgewater",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06752"
+//   },
+//   {
+//     "farm_name": "Horse Listeners Orchard",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "7127",
+//     "website": {
+//       "url": "http://www.horselistenersorchard.com"
+//     },
+//     "zipcode": "06278",
+//     "phone1": "860-429-5336",
+//     "business": "Horse Listeners Orchard",
+//     "l": "23",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.14747618799964, 41.85070942900046]
+//     },
+//     "location_1_address": "317 Bebbington Road",
+//     "location_1_city": "Ashford",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06278"
+//   },
+//   {
+//     "farm_name": "Bywood Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "6088",
+//     "zipcode": "06018",
+//     "phone1": "860-824-5971",
+//     "business": "Bywood Farm",
+//     "l": "11",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-73.25578308899964, 42.03226658700049]
+//     },
+//     "location_1_address": "25 Hollow Brook Lane",
+//     "location_1_city": "Canaan",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06018"
+//   },
+//   {
+//     "farm_name": "Grace Farm",
+//     "category": "Fruit",
+//     "item": "Peaches",
+//     "farmer_id": "16832",
+//     "zipcode": "06489",
+//     "phone1": "860-841-8898",
+//     "business": "Grace Farm",
+//     "l": "10",
+//     "location_1": {
+//       "type": "Point",
+//       "coordinates": [-72.84562375999963, 41.62531353200046]
+//     },
+//     "location_1_address": "747 Flanders Road",
+//     "location_1_city": "Southington",
+//     "location_1_state": "CT",
+//     "location_1_zip": "06489"
+//   }
+// ]
 
+// bring in json file we wrote containing api data that was fetched
+const data = require("./data_from_api.json");
+// fs to write new json file
 const fs = require("fs");
+// geodist is a library that computes the distance between to places using lat and lon points
 const geodist = require("geodist");
 
-// helper function to round decimal points ie: 41.38958856000045 => 41.38
-// function round(num) {
-//   let rounded = num.toFixed(2);
-//   // toFixed returns a string so putting a "+" before rounded turns it into a number
-//   return +rounded;
-// }
-
-// only three farms don't have a business name so we filter thos out
+// only three farms don't have a business name so we filter those out
 // some farm objects don't have "farm name" property so we check "business" property to get farm names
 let farmsWithNames = data.filter((farm) => farm["business"]);
 
-// put farm name and coords into an array
-// a single farm array will look like ["farm name", [latittude, longitude], phone number]
-let farms = farmsWithNames.map((el) => [
-  el.business,
-  [el.location_1.coordinates[1], el.location_1.coordinates[0]],
-  el.phone1,
-]);
+// put farm name, coords, and phone num into an array of objects
+// a single farm object will look like
+// {
+// name: 'Karabin Farms',
+// coords: { lat: 41.61609643000048, lon: -72.82920259099967 },
+// phone: '860-620-0194'
+// }
 
-// create graph to hold farms
+let farms = farmsWithNames.map((farm) => ({
+  name: farm.business,
+  coords: {
+    lat: farm.location_1.coordinates[1],
+    lon: farm.location_1.coordinates[0],
+  },
+  phone: farm.phone1,
+}));
+
+// object to hold farms and the farms that are within 5 miles disance
 let graph = {};
 
-// place each farm into graph with name as key and [ {coords}, [nearby farms], phone num ] as values
+// place each farm into the object with the farm name as a key it's value as { phone num, [nearby farms] }
 for (let i = 0; i < farms.length; i++) {
-  let fName = farms[i][0];
-  let [fCoords1, fCoords2] = farms[i][1];
-  let phoneNum = farms[i][2];
+  let farmName = farms[i].name;
+  let phone = farms[i].phone;
 
-  // let roundedLat = round(fCoords1);
-  // let roundedLon = round(fCoords2);
-
-  graph[fName] = [{ lat: fCoords1, lon: fCoords2 }, [], phoneNum];
+  graph[farmName] = {
+    phone,
+    closeFarms: [],
+  };
 }
 
-// for each farm loop over list of farms and compare lat and lon to determine distance from each other
-// max(farm[i] lattitude, farm[j] lattitude) - min(farm[i] lattitude, farm[j] lattitude) for distance between lat
-// max(farm[i] longitude, farm[j] longitude) - min(farm[i] longitude, farm[j] longitude) for distance between lon
-// With this info we can start making graph connections by checking the distances we calculated to see which farms are nearby
-
-// problem with nested for loops
-// 1. Farm will check itself when looping with j
-// 2. O(n^2) - Slow runtime
-
+// for each farm loop over every other farm and check distance from each other - make connections for farms that are close
 for (let i = 0; i < farms.length; i++) {
   for (let j = 0; j < farms.length; j++) {
-    if (farms[j][0] == farms[i][0]) {
+    if (farms[j] == farms[i]) {
       continue;
     }
 
-        let dist = geodist({
-      lat: farms[i][1][0], lon: farms[i][1][1]
-    }, {
-      lat: farms[j][1][0], lon: farms[j][1][1]
-    })
-
-    // was trying to calculate distance myself, ended up using a library that does this called "geodist"
-    // Math.max(farms[i][1][1], farms[j][1][1]) - Math.min(farms[i][1][1], farms[j][1][1]) < 0.1 &&
-    // Math.max(farms[i][1][0], farms[j][1][0]) - Math.min(farms[i][1][0], farms[j][1][0]) < 0.1
-
+    // using geodist library to find distance between farms
+    let dist = geodist(
+      { lat: farms[i].coords.lat, lon: farms[i].coords.lon },
+      { lat: farms[j].coords.lat, lon: farms[j].coords.lon }
+    );
+    // if farm i & farm j are less than 5 miles away from each otehr - add farm j to farm i's list of nearby farms
     if (dist < 5) {
-      console.log('nearby farm added')
-      graph[farms[i][0]][1].push(farms[j]);
+      console.log("nearby farm added");
+      graph[farms[i].name].closeFarms.push(farms[j].name);
     }
   }
 }
+
+// We now have one big object that contains each farm, it's phone number to 
 
 fs.writeFile("./farm_graph.json", JSON.stringify(graph, null, 1), (err) => {
   if (err) {
@@ -76,22 +370,3 @@ fs.writeFile("./farm_graph.json", JSON.stringify(graph, null, 1), (err) => {
   }
   console.log("File created");
 });
-
-// Loop over array of farms
-// Looking for farms that are 0.2 points away
-
-// for (let farm of farms) {
-//   let closeLat = round(farm[1][1] - 0.2);
-//   let closeLon = round(farm[1][0] - 0.2);
-
-//   // let vector = round(closeLat - closeLon)
-
-//   if (latGraph[closeLat]) {
-//     latGraph[closeLat][1].push(farm);
-//     latGraph[closeLat][1].push(farm);
-//   }
-
-//   if (lonGraph[closeLon]) {
-//     lonGraph[closeLon][1].push(farm);
-//   }
-// }
--- index.js
+++ index.js
@@ -1,67 +1,26 @@
 const fs = require("fs");
-const https = require("https");
-// const axios = require("axios");
+const axios = require("axios");
 
 const apiURL = "https://data.ct.gov/resource/y6p2-px98.json?category=Fruit&item=Peaches";
 
-const apiToJSON = (url) => {
-  let data = "";
-  https
-    .get(url, (resp) => {
-      resp.on("data", (chunk) => {
-        // data you've recieved + the new chunk
-        data += chunk;
-      });
-
-      resp.on("end", () => {
-        // JSON.parse converts a json string into an object.
-        let parsedData = JSON.parse(data);
-        // take the JSON string and write a file with it ()
-        fs.writeFile("./data_from_api.json", JSON.stringify(parsedData, null, 4), (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log("File created");
-        });
-      });
-    })
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
+const getData = async (url) => {
+  try {
+    // data will be in response.data so we can destructure using -> {data} = await axios.get()
+    let { data } = await axios.get(url);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-apiToJSON(apiURL);
-
-
-
-
-
-
-// ==============
-// AXIOS VERSION
-// ==============
-
-// cleaner but uses a library
-
-// const getData = async (url) => {
-//   try {
-      // data will be in response.data so we can destructure using -> {data} = axios.get()
-//     let { data } = await axios.get(url);
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// getData(url)
-//   .then((data) =>
-//     fs.writeFile("./data3.json", JSON.stringify(data, null, 4), (err) => {
-//       if (err) {
-//         console.error(err);
-//         return;
-//       }
-//       console.log("File created");
-//     })
-//   )
-//   .catch((err) => console.error(err));
+getData(apiURL)
+  .then((data) =>
+    fs.writeFile("./data_from_api.json", JSON.stringify(data, null, 4), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("File created");
+    })
+  )
+  .catch((err) => console.error(err));
